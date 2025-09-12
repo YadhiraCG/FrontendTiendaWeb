4c11ed6cf126ba5ba4ac2d1b3b3cd33bed79296d
Yadhira <cerverayadhira@gmail.com>
2025-09-12 11:57:10 -0500

    fix: Correccion de visualizacion del proyecto

---
+++ src/pages/landingPage.tsx
@@ -0,0 +1,14 @@
+import Footer from '../components/footer';
+import Header from '../components/header';
+
+function LandingPage() {
+  return (
+    <div>
+        <Header />
+      <h1>Landing Page</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default LandingPage
