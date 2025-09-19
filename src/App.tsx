6dc7602997942f67b59daeac6a8e0e3c7e3ffe83
Rolando Vega <rolandofraviocegaino@gmail.com>
2025-09-18 20:12:09 -0500

    Desarrollo products

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import LandingPage from './pages/landingPage';
 import Carrito from './pages/carrito';
 import Perfil from './pages/perfil';
+import Products from './pages/products';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/carrito" element={<Carrito />} />
-      <Route path="/perfil" element={<Perfil />} />
-    </Routes>
-  </BrowserRouter>
+      <Header />
+      
+      <main style={{ minHeight: '80vh'}}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </main>
+      
+      <Footer />
+    </BrowserRouter>
   )
 }
 
